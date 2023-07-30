63380a48297aced014eabe26220742e87dbe6f6e
sch0lars <tyler@DESKTOP-1Q79QBA>
2023-07-30 04:43:02 -0700

    Added Initial Syntax Table
    - Created initial syntax table
    - Created initial font locks

--- koopa-mode.el
+++ koopa-mode.el
@@ -1,8 +1,9 @@
 ;;; koopa-mode.el --- A major mode for PowerShell
 
-;; Define the mode
-(define-derived-mode koopa-mode prog-mode "A major mode for PowerShell"
-  "A major mode for editing PowerShell scripts ."
+;;; Code:
+;; Define koopa-mode
+(define-derived-mode koopa-mode prog-mode "A major mode for Microsoft PowerShell"
+  "A major mode for editing PowerShell scripts."
   ;; Set the syntax table
   (setq-local syntax-table koopa-mode-syntax-table)
   ;; Set the font-lock keywords
@@ -11,21 +12,48 @@
 
 ;; Define the syntax table
 (defvar koopa-mode-syntax-table
-  (let ((table (make-syntax-table)))
-    ;; Modify the syntax table according to PowerShell syntax rules
-    ;; For example, you can set characters as word constituents, comments, etc.
-    table
-    )
+  (let ((st (make-syntax-table)))
+    ;; Comment syntax: # starts a comment until the end of the line
+    (modify-syntax-entry ?# "<" st)
+    (modify-syntax-entry ?\n ">" st)
+
+    ;; String syntax: " denotes a string
+    (modify-syntax-entry ?\" "\"" st)
+
+    ;; Variable syntax: $ denotes a variable
+    (modify-syntax-entry ?$ "w" st)
+
+    ;; Brace syntax: (), [], and {} denote braces
+    (modify-syntax-entry ?\( "()" st)
+    (modify-syntax-entry ?\[ "(]" st)
+    (modify-syntax-entry ?\{ "(}" st)
+
+    st)
   "Syntax table for `koopa-mode`.")
+
+; Ignore case for regular expressions
+(setq-default case-fold-search t)
 
 ;; Define the font-lock keywords
 (defvar koopa-mode-font-lock-keywords
-  '(;; Define your font-lock rules here
-    ;; For example, highlight keywords, variables, cmdlets, etc.
-    )
+  ; Highlight comments starting with #
+  '(("\\(#.*\\)" 1 font-lock-comment-face)
+    ; Highlight variables starting with $
+    ("\\($[a-zA-Z_][a-zA-Z0-9_]*\\)" 1 font-lock-variable-name-face)
+    ; Highlight control flow keywords
+    ("\\<\\(for\\|if\\|else\\|elseif\\|switch\\|foreach\\|while\\|do\\)\\>" . font-lock-keyword-face)
+    ; Highlight loop control keywords
+    ("\\<\\(break\\|continue\\)\\>" . font-lock-keyword-face)
+    ; Highlight script block keywords
+    ("\\<\\(function\\|param\\|return\\)\\>" . font-lock-keyword-face)
+    ; Highlight miscellaneous keywords
+    ("\\<\\(begin\\|process\\|end\\)\\>" . font-lock-keyword-face)
+    ; Highlight PowerShell cmdlets
+    ("\\<[a-zA-Z]+\\-[a-zA-Z]+" . font-lock-function-name-face))
   "Keyword highlighting specification for `koopa-mode`.")
 
 ;; Add any keybindings or other customizations here as needed
 ;; For example, defining indentation rules, keybindings for common commands, etc.
 
 (provide 'koopa-mode)
+;;; koopa-mode.el ends here
