50281c01bb9976643801301a1958beb45505520a
sch0lars <sch0lars@users.noreply.github.com>
2023-07-31 04:14:55 -0700

    Added Font-Locking for DotNet Objects

--- koopa-mode.el
+++ koopa-mode.el
@@ -1,13 +1,24 @@
-;;; koopa-mode.el --- A major mode for PowerShell
+;;; koopa-mode.el --- A major mode for Microsoft PowerShell
 
 ;;; Code:
 ;; Define koopa-mode
-(define-derived-mode koopa-mode prog-mode "A major mode for Microsoft PowerShell"
-  "A major mode for editing PowerShell scripts."
+(define-derived-mode koopa-mode prog-mode "koopa-mode"
+  "A major mode for editing Microsoft PowerShell scripts."
   ;; Set the syntax table
   (setq-local syntax-table koopa-mode-syntax-table)
   ;; Set the font-lock keywords
   (setq-local font-lock-defaults '(koopa-mode-font-lock-keywords))
+  ; Make font-lock keywords case-insensitive
+  (set (make-local-variable 'font-lock-defaults)
+       '(koopa-mode-font-lock-keywords nil t))
+  ;; Enable company-mode for auto-completion
+  (company-mode 1)
+  ;; Set company-backends for PowerShell buffer
+  (setq-local company-backends '(company-files company-capf))
+
+  ;; Keybindings
+  ;; Bind run-powershell to C-c C-p
+  (local-set-key (kbd "C-c C-p") 'run-powershell)
   )
 
 ;; Define the syntax table
@@ -16,7 +27,7 @@
     ;; Comment syntax: # starts a comment until the end of the line
     (modify-syntax-entry ?# "<" st)
     (modify-syntax-entry ?\n ">" st)
-
+   
     ;; String syntax: " denotes a string
     (modify-syntax-entry ?\" "\"" st)
 
@@ -31,15 +42,19 @@
     st)
   "Syntax table for `koopa-mode`.")
 
-; Ignore case for regular expressions
-(setq-default case-fold-search t)
+; Ignore case with font-lock regular expressions
+; (setq-local font-lock-keywords-case-fold-search t)
 
 ;; Define the font-lock keywords
 (defvar koopa-mode-font-lock-keywords
   ; Highlight comments starting with #
   '(("\\(#.*\\)" 1 font-lock-comment-face)
-    ; Highlight variables starting with $
-    ("\\($[a-zA-Z_][a-zA-Z0-9_]*\\)" 1 font-lock-variable-name-face)
+    ; Highlight multi-line comments
+    ; ("<#[^#]*[^>]*#>" font-lock-comment-face)
+    ; Highlight variables that start with a $
+    ("\\<\\$[a-zA-Z_][a-zA-Z0-9_]*\\>" . font-lock-variable-name-face)
+    ; Highlight objects from the DotNet framework
+    ("\\[[a-zA-Z0-9_\\.]+\\]:\\{2\\}[a-zA-Z0-9_\\.]+" . font-lock-builtin-face)
     ; Highlight control flow keywords
     ("\\<\\(for\\|if\\|else\\|elseif\\|switch\\|foreach\\|while\\|do\\)\\>" . font-lock-keyword-face)
     ; Highlight loop control keywords
@@ -51,11 +66,26 @@
     ; Highlight PowerShell cmdlets
     ("\\<[a-zA-Z]+\\-[a-zA-Z]+" . font-lock-function-name-face)
     ; Highlight member accessors
-    ("\\<\\(\\$[a-zA-Z0-9_]+\\.[a-zA-Z0-9_]+\\)\\|\\(\\[[a-zA-Z0-9_]+\\]:\\{2\\}[a-zA-Z0-9_]+\\)\\>" . font-lock-function-name-face))
+    (("\\(\\$[a-zA-Z0-9_]+\\.\\)\\([a-zA-Z0-9_]+\\)"  (2 font-lock-function-name-face nil t))))
   "Keyword highlighting specification for `koopa-mode`.")
 
-;; Add any keybindings or other customizations here as needed
 ;; For example, defining indentation rules, keybindings for common commands, etc.
+
+
+;; Define the PowerShell inferior shell buffer
+(defvar powershell-buffer-name "*PowerShell*")
+
+(defun run-powershell ()
+  "Run PowerShell in an inferior shell buffer."
+  (interactive)
+  (let ((powershell-program "powershell.exe"))
+    (pop-to-buffer (get-buffer-create powershell-buffer-name))
+    (unless (comint-check-proc powershell-buffer-name)
+      (apply 'make-comint-in-buffer "PowerShell" powershell-buffer-name powershell-program nil '("-NoExit" "-Command" "-")))
+    (setq-local company-backends '(company-capf))))
+
+;; Set up PowerShell buffer for auto-completion in koopa-mode
+(add-hook 'koopa-mode-hook (lambda () (setq-local company-backends '(company-files company-capf))))
 
 (provide 'koopa-mode)
 ;;; koopa-mode.el ends here
